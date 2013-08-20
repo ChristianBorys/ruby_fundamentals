44680795c9e781a5cb7f01e1340309c1d4e7ab48
inframatic <isteffy@hotmail.com>
2013-08-20 19:43:04 -0400

    hiringprogrammers.rb

---
+++ hiringprogrammers.rb
@@ -0,0 +1,3 @@
+is_an_experienced_ruby_programmer =
+  (candidate.languages_worked_with.include? 'Ruby') &&
+  (candidiate.years_of_experience == >=2 || candidate.github_points >= 500) && ! (candidate.age <15 || candidates_who_applied_recently?)
